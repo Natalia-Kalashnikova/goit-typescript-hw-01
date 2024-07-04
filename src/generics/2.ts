69d448d55edde4850bfd2e4fb6a4edff62bbbff7
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-07-04 03:16:44 +0300

    add-generics

--- src/generics/2.ts
+++ src/generics/2.ts
@@ -1,20 +1,27 @@
-// У вас є тип AllType.Існує функція compare, яка приймає два об'єкти. 
-// Ці об'єкти містять поля AllType. Ваше завдання – використовувати Pick та generics для вказівки, 
-// що поля цих параметрів належать AllType.Функція compare повинна повертати AllType.
+// У вас є тип AllType.Існує функція compare,
+//яка приймає два об'єкти.
+// Ці об'єкти містять поля AllType.
+//Ваше завдання – використовувати Pick та generics для вказівки,
+// що поля цих параметрів належать AllType.
+//Функція compare повинна повертати AllType.
 
 
-// type AllType = {
-//   name: string;
-//   position: number;
-//   color: string;
-//   weight: number
-// }
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
 
-// function compare (top, bottom): AllType {
-//   return {
-//     name: top.name,
-//     color: top.color,
-//     position: bottom.position,
-//     weight: bottom.weight,
-//   }
-// }
+function compare<T extends AllType, U extends AllType>(
+  top: Pick<T, keyof AllType>,
+  bottom: Pick<U, keyof AllType>
+): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
+
